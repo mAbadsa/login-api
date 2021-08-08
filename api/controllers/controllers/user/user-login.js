cba604f45117834d857dcbd84e8fdb1da7f323df
Muhammad <moh22389@gmail.com>
2021-08-08 13:26:17 +0300

    Import initialize database connection function Check is user exists return the respones with user's data

--- api/controllers/controllers/user/user-login.js
+++ api/controllers/controllers/user/user-login.js
@@ -6,15 +6,14 @@ module.exports = {
       required: true,
       type: 'string',
       example: 'asd123',
-      description: 'The user name.',
+      description: 'The username to use for the login account.',
     },
 
     password: {
       required: true,
       type: 'string',
-      maxLength: 200,
       example: 'passwordlol',
-      description: 'The unencrypted password to use for the new account.',
+      description: 'The unencrypted password to use for the login account.',
     },
   },
 
@@ -32,5 +31,80 @@ module.exports = {
     },
   },
 
-  fn: async function ({ password, username }, exits) {},
+  fn: async function ({ password, username }, exits) {
+    const JWT = require('jsonwebtoken');
+
+    const db = sails.config.datastores.default.firebaseAdmin;
+
+    const docRef = db().collection('users');
+
+    // Get user by username
+    const isDocExistByUsername = await docRef
+    .where('username', '==', username)
+    .get();
+
+    // Check is this user was registerd
+    if (!isDocExistByUsername.size) {
+      return exits.invalid();
+    }
+
+    let user = {};
+    // const snapshot = await docRef.get();
+    // snapshot.forEach((doc) => {
+    //   console.log({ doc: doc.id });
+    //   user.id = doc.id;
+    // });
+
+    // Get user data and add it to the user object
+    isDocExistByUsername.forEach((querySnapshot) => {
+      user = { ...user, ...querySnapshot._fieldsProto };
+    });
+
+    // Check if the password is correct
+    const isMatch = await sails.helpers
+      .comparePassword(password, user.password.stringValue)
+      .intercept('password', 'Passwort not match!');
+
+    if (!isMatch) {
+      return this.res.status(401).json({
+        success: false,
+        message: 'Passwprd not match!'
+      });
+    }
+
+    // Sign token with user data
+    const token = await JWT.sign(
+      { username: user.username.stringValue },
+      sails.config.custom.JWT_SECRET_KEY,
+      {
+        expiresIn: sails.config.custom.JWT_EXPIRE,
+      }
+    );
+
+    const options = {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+    };
+
+    // Return success response to the client
+    this.res
+      .status(200)
+      .cookie('token', token, options)
+      .json({
+        success: true,
+        status: 200,
+        message: 'User login successfully.',
+        token,
+        user: {
+          username: user.username.stringValue,
+          email: user.email.stringValue,
+          age: user.age.stringValue,
+          role: user.role.stringValue
+        },
+      });
+
+    // return exits.success('User loged in successfully!');
+  },
 };
