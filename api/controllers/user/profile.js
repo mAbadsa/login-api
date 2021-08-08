5218d17895c62d1fe4503ecf5aa36e8335e4db07
Muhammad <moh22389@gmail.com>
2021-08-08 14:23:53 +0300

    Get loged in user data Get token from request header and verifying token to get user data Fetch user data from database and return by respones

--- api/controllers/user/profile.js
+++ api/controllers/user/profile.js
@@ -15,5 +15,59 @@ module.exports = {
   },
 
   fn: async function () {
+    //Connect to database
+    const db = sails.config.datastores.default.firebaseAdmin;
+    const docRef = db().collection('users');
+
+    // Get token from request header
+    const token = this.req.headers['x-access-token'].split(' ')[1];
+    // verifying token
+    const decoded = await sails.helpers.verifying(token);
+
+    console.log({ decoded });
+    // Check if token is correct
+    if (!decoded) {
+      this.res
+        .status(401)
+        .json({ success: false, message: 'User unthorized!' });
+    }
+
+    // Fetch user from database
+    const getUserByUsername = await docRef
+      .where('username', '==', decoded.username)
+      .get();
+
+    let user;
+
+    // Check if user is exist
+    if (!getUserByUsername.size) {
+      return this.res
+        .status(401)
+        .json({ success: true, message: 'User not exists!' });
+    }
+
+    // Put user data into user object
+    getUserByUsername.forEach((querySnapshot) => {
+      user = querySnapshot._fieldsProto;
+      // console.log(querySnapshot);
+    });
+
+    console.log({ user });
+
+
+    // if (this.req.me) {
+    //   throw {redirect: '/'};
+    // }
+
+    // return {};
+    // Return success response with user data
+    return this.res.status(200).json({
+      success: true,
+      user: {
+        username: user.username.stringValue,
+        email: user.email.stringValue,
+        age: user.age.integerValue,
+      },
+    });
   },
 };
