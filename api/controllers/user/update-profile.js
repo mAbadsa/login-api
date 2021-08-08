37a84fd394b7d9273ed36dd1a2769ac3b40f6c9f
Muhammad <moh22389@gmail.com>
2021-08-08 15:02:23 +0300

    add the inputs with validation

--- api/controllers/user/update-profile.js
+++ api/controllers/user/update-profile.js
@@ -2,6 +2,35 @@ module.exports = {
   friendlyName: 'Signup',
   description: 'Sign up new user',
   inputs: {
+    username: {
+      type: 'string',
+      required: true,
+      example: 'asd123',
+      description: 'The user name.',
+      unique: true,
+    },
+
+    email: {
+      type: 'string',
+      isEmail: true,
+      unique: true,
+      description: 'The email address for the new account, e.g. m@example.com.',
+      extendedDescription: 'Must be a valid email address.',
+    },
+
+    password: {
+      type: 'string',
+      maxLength: 200,
+      minLength: 6,
+      example: 'passwordlol',
+      description: 'The unencrypted password to use for the new account.',
+      regex: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/i,
+    },
+    age: {
+      type: 'number',
+      max: 50,
+      min: 18,
+    }
   },
 
   exits: {
@@ -16,6 +45,18 @@ module.exports = {
       extendedDescription:
         'If this request was sent from a graphical user interface, the request ' +
         'parameters should have been validated/coerced _before_ they were sent.',
+    },
+    emailAlreadyInUse: {
+      statusCode: 409,
+      description: 'The provided email address is already in use.',
+    },
+    usernameAlreadyInUse: {
+      statusCode: 409,
+      description: 'The provided username address is already in use.',
+    },
+    age: {
+      statusCode: 409,
+      description: 'The provided age must be less than 50 and greater than 18.',
     },
   },
 
